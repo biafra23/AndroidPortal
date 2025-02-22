963f613e257391f6c66c4ab240a148bd8467df40
Dirk Jäckel <github@dirk.jaeckel.name>
2025-02-22 03:14:19 +0000

    Added bootnodes resource

--- app/src/main/java/com/jaeckel/androidportal/AndroidPortalApplication.kt
+++ app/src/main/java/com/jaeckel/androidportal/AndroidPortalApplication.kt
@@ -2,8 +2,10 @@ package com.jaeckel.androidportal
 
 import android.app.Application
 import com.arcao.slf4j.timber.BuildConfig
+import org.apache.logging.log4j.LogManager
 import org.slf4j.LoggerFactory
 import timber.log.Timber
+
 
 class AndroidPortalApplication : Application() {
     val logger = LoggerFactory.getLogger(AndroidPortalApplication::class.java)
@@ -19,8 +21,14 @@ class AndroidPortalApplication : Application() {
         }
         logger.debug("-----> Hello DEBUG")
         logger.info("-----> Hello INFO")
-        logger.error("-----> Hello ERROR")
         logger.warn("-----> Hello WARN")
+        logger.error("-----> Hello ERROR")
 
+//        val inputStream = Thread.currentThread().contextClassLoader.getResourceAsStream("bootnodes.json")
+//        val content = inputStream?.bufferedReader().use { it?.readText() }
+//        logger.error("-----> inputStream: $content")
+
+//        val otherLogger = LogManager.getLogger()
+//        otherLogger.error("Hello World")
     }
 }
