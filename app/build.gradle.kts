614e3faa27f30afcf22b3e979cc9ffab5ba87a49
Dirk Jäckel <dirk@jaeckel.com>
2025-07-31 10:07:05 +0200

    cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,35 +92,10 @@ dependencies {
     implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
-//    implementation("org.apache.tuweni:tuweni-bytes:2.3.1")
-//    implementation("io.tmio:tuweni-bytes:2.4.2")
-//    implementation("io.tmio:tuweni-crypto:2.4.2")
-//    implementation("io.tmio:tuweni-units:2.4.2")
-//    implementation("io.tmio:tuweni-rlp:2.4.2")
     implementation("io.projectreactor:reactor-core:3.6.7")
     implementation("io.netty:netty-all:4.1.115.Final")
-//    implementation("tech.pegasys.teku.internal:bytes:24.8.0") {
-//        exclude(group = "org.apache.tuweni", module = "tuweni-bytes")
-//    }
 
     implementation("com.jaeckel.samba:samba-android-wrapper:1.0.0-SNAPSHOT")
-
-    implementation("tech.pegasys.discovery:discovery:develop") {
-//    implementation("com.github.biafra23:discovery:master-SNAPSHOT") {
-//    implementation("tech.pegasys:discovery:25.4.0")
-//    implementation("com.github.Consensys:discovery:25.4.0") {
-//        exclude(group = "org.bouncycastle", module = "bcprov-jdk18on")
-        exclude(group = "org.apache.tuweni", module = "tuweni-rlp")
-        exclude(group = "org.apache.tuweni", module = "tuweni-crypto")
-        exclude(group = "org.apache.tuweni", module = "tuweni-bytes")
-        exclude(group = "org.apache.tuweni", module = "tuweni-units")
-        exclude(group = "org.apache.tuweni", module = "tuweni-io")
-        exclude(group = "io.tmio", module = "tuweni-rlp")
-        exclude(group = "io.tmio", module = "tuweni-crypto")
-        exclude(group = "io.tmio", module = "tuweni-bytes")
-        exclude(group = "io.tmio", module = "tuweni-units")
-        exclude(group = "io.tmio", module = "tuweni-io")
-    }
 
     implementation("org.bouncycastle:bcprov-jdk18on:1.78.1")
     implementation("org.bouncycastle:bcutil-jdk18on:1.78.1")
@@ -143,20 +118,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    implementation("net.java.dev.jna:jna:4.4.0@aar")
-    implementation("io.projectreactor:reactor-core:3.5.0")
-//    implementation("org.rocksdb:rocksdbjni:9.10.0")
-//    implementation("io.maryk.rocksdb:rocksdb-android:9.7.3")
-    implementation("io.maryk.rocksdb:rocksdb-android:9.10.1")
-        // NonSerializableMemoizingSupplier stopped implementing java.util.function.Supplier in 21.0
-    implementation("com.google.guava:guava:33.4.0-android")
-//    implementation("com.google.guava:guava:33.4.0-jre")
-
-    implementation("io.javalin:javalin:6.2.0")
-    implementation("io.javalin:javalin-rendering:5.6.5")
-    implementation("org.thymeleaf:thymeleaf:3.1.2.RELEASE")
-    implementation("org.webjars:swagger-ui:5.17.14")
-
     implementation("com.github.biafra23:trueblocks-kotlin:main-SNAPSHOT") {
         exclude(group = "ch.qos.logback", module = "logback-classic")
     }
@@ -165,10 +126,7 @@ dependencies {
     implementation("com.github.komputing.khex:core:1.1.2")
     implementation("com.github.komputing.khex:extensions:1.1.2")
 
-    implementation("io.consensys.tuweni:tuweni-bytes:2.7.0") {
-//        exclude(group = "org.apache.tuweni", module = "tuweni-bytes")
-//        exclude(group = "io.tmio", module = "tuweni-bytes")
-    }
+    implementation("io.consensys.tuweni:tuweni-bytes:2.7.0")
     implementation("tech.pegasys.teku.internal:unsigned:24.8.0") {
         exclude(group = "org.apache.logging.log4j", module = "log4j-slf4j-impl")
     }
